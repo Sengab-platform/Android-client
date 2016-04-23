9b470a816491a23dc00923684e711364cac9a42e
Rashwan <ahmed.t.rashwan@gmail.com>
2016-04-23 22:28:13 +0200

    Add Navigation Drawer UI.

--- app/src/main/java/com/sengab/MainActivity.java
+++ app/src/main/java/com/sengab/MainActivity.java
@@ -80,17 +80,15 @@ public class MainActivity extends AppCompatActivity
         // Handle navigation view item clicks here.
         int id = item.getItemId();
 
-        if (id == R.id.nav_camera) {
+        if (id == R.id.home_screen) {
             // Handle the camera action
-        } else if (id == R.id.nav_gallery) {
+        } else if (id == R.id.categories){
 
-        } else if (id == R.id.nav_slideshow) {
+        } else if (id == R.id.enrolled_projects) {
 
-        } else if (id == R.id.nav_manage) {
+        } else if (id == R.id.my_projects) {
 
-        } else if (id == R.id.nav_share) {
-
-        } else if (id == R.id.nav_send) {
+        } else if (id == R.id.settings) {
 
         }
 
